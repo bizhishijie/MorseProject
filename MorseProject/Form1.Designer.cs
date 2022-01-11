8d65753a4347bd17b72b4cea217588f32c6d8030
bizhishijie <1031761877@qq.com>
2022-01-11 12:55:52 +0800

    repair bugs

--- MorseProject/Form1.Designer.cs
+++ MorseProject/Form1.Designer.cs
@@ -169,6 +169,7 @@ namespace MorseProject
             this.Controls.Add(this.labelPort);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
